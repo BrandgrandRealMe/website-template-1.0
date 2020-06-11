5355a9692cea8d8993484163c5bcf713c6393251
Glitch (hello-express) <none>
2020-06-11 14:13:18 +0000

    🌘🎟 Checkpoint ./pages/home.html:3235297/168 ./server.js:3235297/138 ./public/style.css:3235297/54 ./public/script.js:3235297/1310

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ app.use(express.static("public"));
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/pages/home.html");
 });
+app.get("/gamevault", (request, response) => {
+  response.sendFile(__dirname + "/pages/gamevalt.html");
+});
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
