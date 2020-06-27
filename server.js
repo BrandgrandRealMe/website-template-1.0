083fa4d2d4cb065fa60e3b0302e8a79ce6e3bb05
Glitch (hello-express) <none>
2020-06-27 23:24:02 +0000

    💶🍢 Checkpoint ./server.js:3235297/510 ./manifest.json:3235297/341 ./README.md:3235297/1014 ./pages/home.html:3235297/8099 ./pages/mit.html:3235297/4892 ./pages/tenplate.html:3235297/15305 ./package.json:3235297/1259 ./public/style.css:3235297/6692

--- server.js
+++ server.js
@@ -1,23 +1,8 @@
 const express = require("express");
 const app = express();
 app.use(express.static("public"));
-app.get("/h", (request, response) => {
-  response.sendFile(__dirname + "/pages/home.html");
-});
-app.get("/gamevault", (request, response) => {
-  response.sendFile(__dirname + "/pages/gamevalt.html");
-});
-app.get("/chat", (request, response) => {
-  response.sendFile(__dirname + "/pages/chat.html");
-});
-app.get("/mit", (request, response) => {
-  response.sendFile(__dirname + "/pages/mit.html");
-});
-app.get("/videos/all", (request, response) => {
-  response.sendFile(__dirname + "/videos/all.html");
-});
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/videos/template.html");
+  response.sendFile(__dirname + "/pages/home.html");
 });
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
